706751399d0050737e8fb12ee24ca7158a1ec57f
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-02 20:16:34 +0530

    Factors of Number

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+package com.company.basicprogram;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int num = scanner.nextInt();
+        int fact = 1;
+        for (int i = 1; i<=num; i++){
+            fact = fact * i;
+        }
+        System.out.println("Factorial of " +num+ " is " +fact);
+    }
+}
